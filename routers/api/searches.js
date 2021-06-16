9c4cc9078cf16d3f1ef867c99f4163760dbdaf71
Albert Baiges <albertbaigesjabega@gmail.com>
2021-06-16 15:23:26 +0200

    Added try-catch to filter endpoint

--- routers/api/searches.js
+++ routers/api/searches.js
@@ -4,15 +4,18 @@ const router = express.Router();
 const { searchesController } = require("../../controllers");
 
 router.get("", async (req, res) => {
-    const data = await searchesController.getUsers(req.query);
-    let response;
-    if(data) {
-        response = {users: data};
-    } else {
-        response = {error: "No users matched the filter"}
+    try {
+        const data = await searchesController.getUsers(req.query);
+        let response;
+        if(data) {
+            response = {users: data};
+        } else {
+            response = {error: "No users matched the filter"};
+        }
+        res.json(response);
+    } catch (error) {
+        return res.status(500).json({"Error": error.message});   
     }
-    res.json(response)
-    console.log(response)
 });
 
 
